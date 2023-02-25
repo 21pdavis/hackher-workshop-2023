a6a1a012f69d554571a0d50309106c29e9bd146f
21pdavis <paulcdavis135@gmail.com>
2023-02-25 14:29:58 -0500

    feat: Basic demo code and command-line run mode parsing
    Implemented the demo code found in the home page of rust-sdl2's documentation
    and created some basic infrastructure for different run modes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,109 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use colored::Colorize;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::render::Canvas;
+use sdl2::Sdl;
+use std::time::Duration;
+
+fn init_canvas(context: &Sdl) -> Canvas<sdl2::video::Window> {
+    // SDL has different "subsystems" that you can initialize. Here we initialize the video subsystem.
+    // See the docs for more details: https://www.libsdl.org/release/SDL-1.2.15/docs/html/guidebasicsinit.html
+    let video_subsystem = context.video().unwrap();
+
+    let window = video_subsystem
+        .window("HackHer Workshop 2023", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let canvas = window.into_canvas().build().unwrap();
+
+    canvas
+}
+
+fn run_demo(sdl_context: &Sdl) {
+    let mut canvas = init_canvas(&sdl_context);
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        // The rest of the game lnoop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+
+pub fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // we need the ampersand (&) before args to tell the compiler we only want to pass dbg! a temporary reference to args, not "move" the value.
+    dbg!(&args);
+
+    let accepted_args = vec!["demo"];
+
+    // some funky command-line parsing showing off some basic rust string/vector manipulation features
+    if args.len() > 2 {
+        println!("Usage: {} -- <run-mode>", args[0]);
+        return;
+    } else if args.len() == 2 && !accepted_args.contains(&args[1].as_str()) {
+        eprint!(
+            "{} {} {} {} ",
+            "Error:".bold().red(),
+            "argument".red(),
+            args[1].red(),
+            "not in accepted arg list:".red()
+        );
+
+        // for anyone who has taken 220: this map expression might interest you!
+        let mut accepted_args_colored = accepted_args
+            .into_iter()
+            .map(|s| s.to_string().green())
+            .peekable();
+
+        // pretty-printing the list of accepted args colored green
+        while let Some(arg) = accepted_args_colored.next() {
+            eprint!("{}{}", arg, if accepted_args_colored.peek().is_none() { "" } else { ", " });
+        }
+        eprintln!();
+
+        return;
+    }
+
+    // initialize the main OpenGL context for SDL2
+    let sdl_context = match sdl2::init() {
+        Ok(context) => context,
+        Err(err) => {
+            println!(
+                "Something went terribly wrong while initializing SDL!: {}",
+                err
+            );
+            return;
+        }
+    };
+
+    match (args.len(), if args.len() == 2 { args[1].as_str() } else { "" }) {
+        (1, _) => run_demo(&sdl_context),
+        (2, "demo") => run_demo(&sdl_context),
+        _ => println!("No run mode specified, running demo"),
+    }
 }
